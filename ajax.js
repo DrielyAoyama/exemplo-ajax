8a336df33f2c1ca797597cf8347bf87568fcc1b8
daoyama <driely.aoyama@hotmail.com>
2016-09-08 15:32:22 -0300

    exemplo

---
+++ ajax.js
@@ -0,0 +1,31 @@
+function ajaxGET()
+{
+	$.ajax({             
+       type: "GET",
+       data: 
+        { 
+       		nome:'Driely',
+       		endereco: 'Rua: ......, numero: 000',
+       		telefone: '0000000'
+       	},             
+       url: "http://exemplo.com.br",
+    });
+}
+
+
+
+//a requisição será feita sem refresh da pagina na url 
+// http://exemplo.com.br com as variáveis passadas de maneira OCULTA
+function ajaxGET()
+{
+	$.ajax({             
+       type: "POST",
+       data: 
+        { 
+       		nome:'Driely',
+       		endereco: 'Rua: ......, numero: 000',
+       		telefone: '0000000'
+       	},             
+       url: "http://exemplo.com.br",
+    });
+}
