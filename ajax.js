43ad6d759a95297d012aead7270dfbeb5287e9cb
DrielyAoyama <driely.aoyama@hotmail.com>
2016-09-08 15:33:22 -0300

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -16,7 +16,7 @@ function ajaxGET()
 
 //a requisição será feita sem refresh da pagina na url 
 // http://exemplo.com.br com as variáveis passadas de maneira OCULTA
-function ajaxGET()
+function ajaxPOST()
 {
 	$.ajax({             
        type: "POST",
